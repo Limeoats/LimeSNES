9fda667227083f074687eafd65dad8ee97a45706
cw1650 <admin@limeoats.com>
2017-03-08 15:23:49 -0500

    Initial commit

---
+++ html/src/com/limeoats/limesnes/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.limeoats.limesnes.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.limeoats.limesnes.LimeSNES;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LimeSNES();
+        }
+}
